31c2c84189b904bb67c057600ec25aae756dd8eb
emmdeveloper <keshichidera@gmail.com>
2023-11-11 18:05:47 +0100

    nothing-much

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,6 +7,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SERCET,
 });
 
 export { handler as GET, handler as POST };
